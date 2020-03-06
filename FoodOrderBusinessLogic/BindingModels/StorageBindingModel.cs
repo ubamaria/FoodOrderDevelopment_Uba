2c952b1c5ad36f381542a9aa9967933bb02510ce
Мария <uba2014@mail.ru>
2020-03-06 08:13:13 +0400

    hw1

---
+++ FoodOrderBusinessLogic/BindingModels/StorageBindingModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoodOrderBusinessLogic.BindingModels
+{
+    public class StorageBindingModel
+    {
+        public int Id { get; set; }
+        public string StorageName { get; set; }
+        public List<StorageDishBindingModel> StorageDishes { get; set; }
+    }
+}
