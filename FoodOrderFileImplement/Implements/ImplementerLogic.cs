a51fd060dae08bfedca7f1e46847a4cc2f5f918a
Мария <uba2014@mail.ru>
2020-06-05 13:03:30 +0400

    fix

--- FoodOrderFileImplement/Implements/ImplementerLogic.cs
+++ FoodOrderFileImplement/Implements/ImplementerLogic.cs
@@ -20,15 +20,28 @@ namespace FoodOrderFileImplement.Implements
 
         public void CreateOrUpdate(ImplementerBindingModel model)
         {
-            Implementer element = source.Implementers.FirstOrDefault(rec => rec.Id == model.Id);
-
-            if (element == null)
+            Implementer element = source.Implementers.FirstOrDefault(rec => rec.ImplementerFIO == model.ImplementerFIO && rec.Id != model.Id);
+            if (element != null)
             {
-                int maxId = source.Implementers.Count > 0 ? source.Implementers.Max(rec => rec.Id) : 0;
-                element = new Implementer { Id = maxId + 1 };
+                throw new Exception("Уже есть исполнитель с таким именем");
+            }
+            if (model.Id.HasValue)
+            {
+                element = source.Implementers.FirstOrDefault(rec => rec.Id == model.Id);
+                if (model.Id.HasValue)
+                {
+                    if (element == null)
+                    {
+                        throw new Exception("Элемент не найден");
+                    }
+
+                }
+            }
+            else
+            {
+                element = new Implementer();
                 source.Implementers.Add(element);
             }
-
             element.ImplementerFIO = model.ImplementerFIO;
             element.WorkingTime = model.WorkingTime;
             element.PauseTime = model.PauseTime;
